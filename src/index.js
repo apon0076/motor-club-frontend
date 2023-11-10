88a753b55240823fe5403bb70fa40b5be9c9e0fa
apon0076 <s.m.abdullah.kst@gmail.com>
2023-11-11 00:41:48 +0600

    axios interceptors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
-import 'react-responsive-modal/styles.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import "react-responsive-modal/styles.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import axios from "axios";
+
+// Axios Interceptors Request
+axios.interceptors.request.use((request) => {
+  var token = localStorage.getItem("authToken");
+  request.headers = {
+    Authorization: `Bearer ${token}`,
+    // 'Accept': 'application/json',
+    // 'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  return request;
+});
+
+// Axios Interceptors Response
+axios.interceptors.response.use((response) => {
+  console.log("response", response);
+  return response;
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,6 +34,5 @@ ReactDOM.render(
       </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
